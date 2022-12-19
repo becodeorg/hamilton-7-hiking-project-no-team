f95f9632e3d870e158667fef78056ed3b49bc1b4
Crimsm <c.danniau27@gmail.com>
2022-12-19 15:57:34 +0100

    hikes

---
+++ Database/Hikes.sql
@@ -0,0 +1,68 @@
+INSERT INTO Hikes (name, 
+                   distance, 
+                   duration, 
+                   elevation_gain, 
+                   description)
+VALUES('Braine-le-Château et les Monts', 
+       '10.68', 
+       '', 
+       '235', 
+       "Très belle promenade dans une des plus jolies communes du Brabant Wallon. Une partie du parcours se fait en brabant flamand (Lembeek)");
+
+INSERT INTO Hikes (name, 
+       			   distance, 
+       			   duration, 
+       			   elevation_gain, 
+       			   description)
+VALUES('Entre Yvoir et Warnant par le Ravel', 
+       '12.25', 
+       '4:05',
+       '207', 
+       "Randonnée accessible à toutes et tous. Grace à cette randonnée dont le départ et l'arrivée se fait sur la place principale d'Yvoir, découvrez le Ravel vers Warnant.");
+
+INSERT INTO Hikes (name, 
+       			   distance, 
+       			   duration, 
+       			   elevation_gain, 
+       			   description)
+VALUES('Stoumont - Fagne de Crambon', 
+       '12.25', 
+       '3:17', 
+       '313', 
+       "Une bien fort jolie promenade en boucle depuis Stoumont jusqu'à la fagne de Crambon.");
+
+INSERT INTO Hikes (name, 
+       			   distance, 
+       			   duration, 
+       			   elevation_gain, 
+       			   description)
+VALUES('Angleur - Colonster - Sart-Tilman ', 
+       '14.36', 
+       '', 
+       '225', 
+       "Très belle promenade aux portes de Liège, essentiellement à travers bois. Elle passe par la lande de Streupas, rejoint l'Ourthe et court parallèlement à celle-ci jusqu'au château de Colonster, pour revenir par les bois du Sart Tilman.");
+
+INSERT INTO Hikes (name, 
+       			   distance, 
+       			   duration, 
+       			   elevation_gain, 
+       			   description)
+VALUES('Purnode et la vallée du Bock', 
+       '9.80', 
+       '', 
+       '231', 
+       "Superbe promenade dans cette vallée namuroise peu fréquentée, et pourtant splendide. Départ et retour de Purnode, petit village près d'Yvoir.");
+
+INSERT INTO Hikes (name, 
+       			   distance, 
+       			   duration, 
+       			   elevation_gain, 
+       			   description)
+VALUES('le Tour d Olne', 
+       '25', 
+       '8:35', 
+       '275', 
+       "Ce tour d’Olne qui vous emmène, par monts et par vaux, à la découverte d’un des plus beaux villages de Wallonie.");
+
+
+
