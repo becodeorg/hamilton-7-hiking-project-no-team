3c8f062b11dbff82310c4a3b974efa758674e75e
Jérôme Trips <jerome.trips@tutanota.com>
2022-12-20 09:52:54 +0100

    fixed dockerfile and env

--- src/public/index.php
+++ src/public/index.php
@@ -3,5 +3,5 @@ declare(strict_types=1);
 require 'vendor/autoload.php';
 
 $test = new UserModel();
-$test->create('test639', 'Ty', 'Tester', 'test3390@server.com', 'csjhqldkuçoqmpkkdsqqkdmq');
-var_dump($test->find('test639'));
+$test->create('test6399', 'Ty', 'Tester', 'test566@server.com', 'cszzjhqldkuçojjsjssqqkdmq');
+var_dump($test->find('test6399'));
